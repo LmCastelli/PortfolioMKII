d335afdb131164cd5fe92410678abcf7b210a74e
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2024-04-02 18:50:42 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+function App() {
+    return (
+        <div>
+            <NavBar />
+            <h1>Howdy</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,37 @@
+import "./styling/NavBar.css"
+
+
+function NavBar() {
+    
+
+    return (
+        <div className="NavShell">
+            <div className="IconShell">
+                <div className="ImgShell">
+                    <a href="https://github.com/LmCastelli" target="_blank" rel="noreferrer noopener"> <img className="ci ci-github ci-2x" alt="GitHub Icon"/></a>
+                </div>
+                <div className="ImgShell">
+                    <a href="https://www.linkedin.com/in/lucas-castell/" target="_blank" rel="noreferrer noopener"> <img className="ci ci-linkedin ci-2x" alt="LinkedIn Icon"/> </a> 
+                </div>
+                <div className="ImgShell">
+                    <a href="mailto:lucasmcastelli@gmail.com" target="_blank" rel="noopener noreferrer"> <img className="ci ci-gmail ci-2x" alt="Gmail Icon"/> </a>
+                </div>
+            </div>
+            <div className="LinkShell">
+                <div className="SectionShell">
+                    <h3>About</h3>
+                </div>
+                <div className="SectionShell">
+                    <h3>Projects</h3>
+                </div>
+                <div className="SectionShell">
+                    <h3>Contact</h3>
+                </div>
+            </div>
+            
+        </div>
+    )
+
+}
+
+export default NavBar;
