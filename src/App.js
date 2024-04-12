f347cf59aab83e7590303a815cd7dde10e9aab41
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2024-04-11 19:30:36 -0500

    Added images and some urls for projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import "./styling/App.css"
 import NavBar from "./NavBar";
 import Project from "./Project";
 import me from "./Me.jpeg"
+import Encounter from "./Encounter.png"
+import Wordie from "./Wordie.png"
+import WorkIP from "./UnderConstruction.jpg"
+import Cargo from "./Cargo.png"
+import Test1 from "./Test1.png"
+import Test2 from "./Test2.png"
+import Test3 from "./Test3.png"
+
+
+import BlackJack from "./Blackjack.png"
+import Lever from "./Heavy Lever.png"
+
 
 function App() {
     return (
@@ -22,7 +34,15 @@ function App() {
                 </div>
             </div>
             <div className="ProjectShell">
-                <Project title="title" img={me} url="testURL" blurb="yadayada" />
+                <Project title="Encounter Manager" img={Encounter} url="testURL" blurb="Dungeons and Dragons encounter manager that allows the user to store data on monsters, characters, and story" />
+                <Project title="Wordie" img={Wordie} url="https://projects-wordie.netlify.app/" blurb="A World clone I made from scratch, utilizing an API to grab just about any five-letter word, give it a try!" />
+                <Project title="Pickup Basketball" img={WorkIP} url="testURL" blurb="Mockup for a social app that allows users to schedule and find pickup basketball games" />
+                <Project title="Cargo Planner" img={Test1} url="https://cargo-layout.vercel.app/" blurb="Tool that allows users to input truck and cargo specs to calculate optimal truck layouts" />
+
+                <Project title="BlackJack" img={BlackJack} url="testURL" blurb="Pretty standard blackjack game that calculates winning hand as well as keeps track of money and wagers" />
+
+                <Project title="Heavy Lever" img={Lever} url="https://jsfiddle.net/LucasCastelli/ou23wqng/71/" blurb="Fun little animation project I did to learn some animation properties in css as well as play around with z layers" />
+
             </div>
         </div>
     )
--- src/Project.js
+++ src/Project.js
@@ -1,17 +1,17 @@
-
+import "./styling/Project.css"
 function Project({title, blurb, img, url}) {
 
 
     return(
         <div className="ProjectDiv">
-            <div className="ImgShell">
-                <img src={img} alt={title}></img>
+            <div className="ProjectImg">
+                <img className="Screenshot" src={img} alt={title}></img>
             </div>
             <div className="InfoShell">
                 <div className="TitleShell">
                     <a href={url}>{title}</a>
                 </div>
-                <div className="BlurbShell">
+                <div className="ProjectBlurb">
                     <h5>{blurb}</h5>
                 </div>
             </div>
